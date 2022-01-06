00dc56e28e2204eded0d220b022582f3c2715087
Max-Martin Ketzel <max-martin.ketzel@smarttecs.de>
2022-01-06 22:34:33 +0100

    gitignore and creation of first source files

---
+++ src/de/zaby/breakout/BreakOut.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.zaby.breakout;
+
+/**
+ * @author Max-Martin Ketzel
+ *
+ */
+public class BreakOut {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/zaby/breakout/Game.java
@@ -0,0 +1,19 @@
+package de.zaby.breakout;
+
+
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+	
+	// Setup the game
+	@Override
+	public void setup() {
+		
+	}
+	
+	//Update and draw everything in the game
+	@Override
+	public void draw() {
+		
+	}
+}
